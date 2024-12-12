cde8220014a61384992baeada39e6f4ee46c3663
kyuyounglee <pia222.20240820@gmail.com>
2024-12-12 14:14:02 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=q0yk=fp(d*9^9c8!730ib$nhl(9j0y*j_3mg8$6ln@%ab#scp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.125.108.168']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- config/settings/local.py
+++
@@ -1,2 +0,0 @@
-from .base import *  # base.py와 동일한 설정값을 사용
-ALLOWED_HOSTS = []   # ALLOWED_HOSTS 값은 base.py의 값을 사용안하고 재 정의
